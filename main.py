09c294d365ec36fc79979d3e85209fb3ca640cb4
Daniil_Manuilov <manuilovdaniil@yandex.ru>
2023-01-10 18:55:41 +0100

    download books from tululu.org

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from pathlib import Path
+
+
+TUTULU_URL = 'https://tululu.org/txt.php?id='
+
+
+def load_book(book_id):
+    url = ''.join([TUTULU_URL, str(book_id)])
+
+    response = requests.get(url, verify=False)
+    response.raise_for_status()
+
+    return response.text
+
+
+def main():
+    books_dir = Path.cwd().joinpath('books')
+    Path(books_dir).mkdir(exist_ok=True)
+
+    for book_id in range(1, 11):
+        try:
+            book_text = load_book(book_id)
+
+            with open(books_dir.joinpath(f'id{book_id}.txt'), 'w') as file:
+                file.write(book_text)
+
+        except requests.exceptions.HTTPError:
+            print(f'книги с id={id} нет')
+            pass
+
+
+if __name__ == '__main__':
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)  # disable warnings
+    main()
